2fdf03fbecd60b298cb172fc8113030180c109af
Stephanie <stephaniesaavedra425@gmail.com>
2023-09-07 11:52:12 +0800

    Initial commit

---
+++ src/introduction_02.java
@@ -0,0 +1,7 @@
+class introduce {
+    public static void main(String[] args) {
+        System.out.println("Hi! My name is Stephanie N. Saavedra! Hello again");
+        System.out.println("I am a new employee, helloe once more");
+    }
+}
+
