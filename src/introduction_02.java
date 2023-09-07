5a684501ecf13cc409b5ab735aa248ae8b61ba3b
Stephanie <stephaniesaavedra425@gmail.com>
2023-09-07 15:37:56 +0800

    3rd

--- src/introduction_02.java
+++ src/introduction_02.java
@@ -1,7 +1,7 @@
 class introduce {
     public static void main(String[] args) {
         System.out.println("Hi! My name is Stephanie N. Saavedra! Hello again and no goodbye");
-        System.out.println("I am a new employee, hello once more, hehehe");
+        System.out.println("I am a new employee, hello once more");
     }
 }
 
